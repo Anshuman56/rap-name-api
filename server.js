4383a756194da610d4bac5ed205279a7ba65e9f8
Anshuman56 <anshumandehury@yandex.com>
2026-02-17 12:41:42 +0530

    working rapper API

---
+++ server.js
@@ -0,0 +1,41 @@
+let exprss = require('express');
+let app = exprss();
+const PORT = 8000;
+
+const rappers = {
+    '21 savage' :{
+    'age' : 29,
+    'birthName' : 'jubrish' ,
+    'birthLocation': 'London, England',
+},
+'chance the rapper' : {
+    'age': 29,
+    'birthName': 'Chancelor Bennett',
+    'birthLocaiton': 'Chicago, Illinois'
+},
+'unknow':{
+    'age': 0,
+    'birthName': 'unkown',
+    'birthLocation': 'unkown'
+}
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+});
+
+app.get('/api/:name', (request, response) => {
+   const rapperName = request.params.name.toLocaleLowerCase();
+   
+   if(rappers[rapperName]){
+     response.json(rappers[rapperName]);
+   }
+   else{
+    response.json(rappers['unknow'])
+   }
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on the port ${PORT}! Better Go Catch It!`);
+});
